67a848b587784cfe4fbc18e71701d100f08cc909
unknown <codinglife1990@gmail.com>
2024-09-13 23:00:29 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,64 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: " Full Stack Developer",
+    company: "Free Lancing.",
+    description: `Design and implement user interfaces using HTML, CSS, and JavaScript frameworks (e.g., React, Angular, Vue.js).
+Ensure responsive design and cross-browser compatibility.
+Collaborate with designers to translate wireframes and prototypes into, Back- End : Write and maintain code that handles application logic, processes user requests, and performs tasks like authentication, data processing, and business logic.
+Implement and manage APIs (Application Programming Interfaces) that`,
+    technologies: [
+      "Javascript",
+      "React.js",
+      "Next.js",
+      "mongoDB",
+      "Tailwind.css",
+    ],
+  },
+  {
+    year: "2020 - 2023",
+    role: "Sr Vendor Management",
+    company: "Raftaar Technologies Pvt Ltd",
+    description: `Co-managed digital microsite and promotional projects with outside vendors. Created analytic reports to help improve our digital marketing initiatives`,
+    technologies: ["Inbuild software", "Google Sheets", "Admin panel"],
+  },
+  {
+    year: "2017 - 2020",
+    role: "Quality Analyst",
+    company: "Effort BPO Ltd",
+    description: `Developed and maintained Reports using Formulated Excel Sheet, Auditng calls for the 30 plus agents, Giving feedback for the agents and conducting product knowledge test for everyone`,
+    technologies: ["Excel", "Word", "calls editor"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["React", "Node.js", "MongoDB", "Tailwind.css", "Next.js"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["React", "Tailwind.css", "Frame Motion"],
+  },
+];
+
+export const CONTACT = {
+  address: "Near HP Colony, Chembur, Mumbai, 400074 ",
+  phoneNo: "+91 8369 538 779 ",
+  email: "jeevanmairale@gmail.com",
+};
